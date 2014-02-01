69c403da76064962e15dea290f289226ecb41a2e
Kai <im.kaiyu@gmail.com>
2014-02-01 13:48:40 -0500

    stories and blends page

--- js/controllers.js
+++ js/controllers.js
@@ -7,23 +7,134 @@ angular.module('jitteryApp.controllers', [])
 
   // Set our reviews object to be empty by default.
   $scope.reviews = [];
-  $scope.reviewCount = [
-    { "name": "After Dinner", "count": 0, "sum": 0, "average": 0},
-    { "name": "Bourque Newswatch", "count": 0, "sum": 0, "average": 0},
-    { "name": "CN Tower of Power", "count": 0, "sum": 0, "average": 0},
-    { "name": "Connoisseur Estates", "count": 0, "sum": 0, "average": 0},
-    { "name": "Columbian", "count": 0, "sum": 0, "average": 0},
-    { "name": "Fordnation Blend", "count": 0, "sum": 0, "average": 0},
-    { "name": "Hawaii Kona", "count": 0, "sum": 0, "average": 0},
-    { "name": "House Blend", "count": 0, "sum": 0, "average": 0},
-    { "name": "Italian Roast", "count": 0, "sum": 0, "average": 0},
-    { "name": "Lionel Roastie", "count": 0, "sum": 0, "average": 0},
-    { "name": "Mocca-Java", "count": 0, "sum": 0, "average": 0},
-    { "name": "Reggae Blend", "count": 0, "sum": 0, "average": 0},
-    { "name": "Ruth Roast", "count": 0, "sum": 0, "average": 0},
-    { "name": "Toronto Blend", "count": 0, "sum": 0, "average": 0},
-    { "name": "Tropic of Coffee", "count": 0, "sum": 0, "average": 0},
-    { "name": "World Tour Blend", "count": 0, "sum": 0, "average": 0}
+  $scope.reviewCount = [{
+    "name": "After Dinner",
+    "count": 0,
+    "sum": 0,
+    "average": 0,
+    "description": "Europeans often have a coffee after dinner.  Try sampling a taste of Europe here at home, with a soft, fruity blend that will still let you get some beauty sleep at night.",
+    "picture": ""
+  },
+  { 
+    "name": "Bourque Newswatch",
+    "count": 0,
+    "sum": 0,
+    "average": 0,
+    "description": "Our intern named this blend, we honestly don’t know what it was until we googled the name (he wanted to be a journalist).  Try this classic flavour, roasted with cinnamon to give it a new kick!",
+    "picture": ""
+  },
+  { 
+    "name": "CN Tower of Power",
+    "count": 0,
+    "sum": 0,
+    "average": 0,
+    "description": "Did you know it takes half a million coffee beans end to end to reach the height of the CN Tower? Once you’ve tried this blend you’ll have the superpower of the CN tower bursting out of your fingertips in a half a million coffee bean blend.",
+    "picture": ""
+  },
+  { 
+    "name": "Connoisseur Estates",
+    "count": 0,
+    "sum": 0,
+    "average": 0,
+    "description": "This is our most sophisticated and most complex blend yet. It is highly praised by coffee connoisseurs around the world, and we maintain the recipe as a highly coveted secret.  Try it with a pipe for extra effect.",
+    "picture": ""
+  },
+  { 
+    "name": "Columbian",
+    "count": 0,
+    "sum": 0,
+    "average": 0,
+    "description": "Straight from Colombia, this blend is brought straight to you after escaping the local coffee cartels.  With a strong bold flavour, and a light nutty note, enjoy it while you can.",
+    "picture": ""
+  },
+  { 
+    "name": "Fordnation Blend",
+    "count": 0,
+    "sum": 0,
+    "average": 0,
+    "description": "Named after our latest scandalous Toronto Mayor, this blend is only for the daring. It’s a strong blend featuring a bold flavour, and possibly some illegal substances. Try it at your own risk!",
+    "picture": "img/blends/Ford.png"
+  },
+  { 
+    "name": "Hawaii Kona",
+    "count": 0, 
+    "sum": 0, 
+    "average": 0,
+    "description": "Do you miss the beach, surfing and coconut trees?  Try Jittery Joe’s version of tropical paradise in a cup. Featuring a nutty coconut taste and a hint of sweetness, this blend is a vacation at home.",
+    "picture": ""
+  },
+  { 
+    "name": "House Blend",
+    "count": 0,
+    "sum": 0,
+    "average": 0,
+    "description": "Overwhelmed by our huge selection of coffees? Try our classic house blend that’s roasted in house with tonnes of love from Jittery Joe.",
+    "picture": ""
+  },
+  { 
+    "name": "Italian Roast",
+    "count": 0,
+    "sum": 0,
+    "average": 0,
+    "description": "We tried to make a coffee version of an Italian expresso, and I think we succeeded.  This is a rich blend of coffee that resembles the strong taste of espresso that is meant for the bold coffee lovers out there.",
+    "picture": ""
+  },
+  { 
+    "name": "Lionel Roastie",
+    "count": 0,
+    "sum": 0,
+    "average": 0,
+    "description": "Hello!  Is a strong, bold flavour you’re looking for?  If you ever wanted to be reminded of the 80’s, the Lionel Roastie will take you back to the golden age of funky music.",
+    "picture": ""
+  },
+  { 
+    "name": "Mocca-Java",
+    "count": 0,
+    "sum": 0,
+    "average": 0,
+    "description": "Who doesn’t love chocolate?  Our funky Mocca-Java blend is roasted with organic dark cocoa beans which will keep you energized all day.  Just don’t become addicted. ",
+    "picture": ""
+  },
+  { 
+    "name": "Reggae Blend",
+    "count": 0,
+    "sum": 0,
+    "average": 0,
+    "description": "Ya man!  This blend will have you singing “Dont Worry, Be Happy” to its funky flavours.  Have a taste of classic Reggae that will remind you of Jamaica here in at Jittery Joes.",
+    "picture": ""
+  },
+  { 
+    "name": "Ruth Roast",
+    "count": 0,
+    "sum": 0,
+    "average": 0,
+    "description": "Ruth Ruth Ruth!! A blend named after our favourite customer shows you how much we care about your feedback. Tell us which blend is your favourite and you might have a blend named after you.",
+    "picture": ""
+  },
+  { 
+    "name": "Toronto Blend",
+    "count": 0,
+    "sum": 0, 
+    "average": 0,
+    "description": "If you are living in a Toronto state of mind, then this blend is the perfect companion for you.  Its complex yet subtle taste is a true reflection of such a diverse city.",
+    "picture": ""
+  },
+  {
+    "name": "Tropic of Coffee",
+    "count": 0,
+    "sum": 0,
+    "average": 0,
+    "description": "Tropic of Cancer or Tropic of Capricorn? The best place to be is the Tropic of Coffee, we traveled around the world for this exotic blend of tropical beans that will bring you closer to the equator.",
+    "picture": ""
+  },
+  {
+    "name": "World Tour Blend",
+    "count": 0,
+    "sum": 0,
+    "average": 0,
+    "description": "When Jittery Joe was a young lad, he traveled around the world displaying his secret blend composed of 7 different types of coffee beans. This blend had been presented to royal families in Japan, Morocco and Madagascar. Now you can drink like a king with this excellent blend!",
+    "picture": ""
+  }
   ]
   // JSONP to get the current ratings.
   $http.jsonp('http://jitteryjoes.myplanetfellowship.com/api/ratings.jsonp?callback=JSON_CALLBACK').
